54b05b24d4708a569d5a860f42fc239534594202
Maria <maria.lyng@hotmail.com>
2020-11-19 09:32:52 +0100

    parking spots

--- components/Navigation/TopNavigation.js
+++ components/Navigation/TopNavigation.js
@@ -1,8 +1,9 @@
 import React,{ Component } from 'react'
 import { Button, View, Text } from 'react-native';
 import {createStackNavigator} from "@react-navigation/stack";
-import CbsScreen from './../SubComponent/CbsScreen';
-import DetailsScreen from './../SubComponent/Details';
+import ParkingSpots from "./../Screens/ParkingSpots/ParkingSpots";
+import AddParkingSpots from "./../Screens/ParkingSpots/AddParkingSpot";
+
 
 const Stack = createStackNavigator()
 
@@ -11,8 +12,8 @@ export default class TopNavigation extends Component {
     render() {
         return (
             <Stack.Navigator>
-                <Stack.Screen name="Details" component={DetailsScreen} />
-                <Stack.Screen name="CBS" component={CbsScreen} />
+                <Stack.Screen name="Parking Spots" component={ParkingSpots} />
+                <Stack.Screen name="Tilføj parkeringsplads" component={AddParkingSpots} />
             </Stack.Navigator>
         );
     }
