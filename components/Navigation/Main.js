95c79cc6a7f895b1f2d2e35e868302ed6ee1d33b
kalu18ab <kajsjang@gmail.com>
2020-12-08 12:28:16 +0100

    yes

--- components/Navigation/Main.js
+++ components/Navigation/Main.js
@@ -5,43 +5,58 @@ import StackNavigation from "./StackNavigation";
 import Auth from "../Authentication/Auth";
 import TopNavigation from "./TopNavigation";
 import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import ParkingSpots from "../Screens/ParkingSpots/ParkingSpots";
 import Icon from 'react-native-vector-icons/Ionicons';
 
 const Tab = createMaterialBottomTabNavigator();
 
-const MainTabs = () => (
-    <Tab.Navigator tabBarOptions={{
-        activeTintColor: 'tomato',
-        inactiveTintColor: 'gray',
-        labelStyle: {fontSize: 22, padding: 6},
-        style: {height: 80}   
-      }}>
-        <Tab.Screen
-        name="Home"
-        component={HomeScreen}
-        options={{
-            tabBarIcon: ({color}) =>(
-            <Icon name="home" size={30} color={color} />
-            )}}
-        />
-        <Tab.Screen
-        name="Parkingspots"
-        component={TopNavigation}
-        options={{
-            tabBarIcon: ({color}) =>(
-            <Icon name="user-friends" size={30} color={color} />
-            )}}
-        />
-        <Tab.Screen
-        name="Settings"
-        component={StackNavigation}
-        options={{
-            tabBarIcon: ({color}) =>(
-            <MaterialIcons name="settings" size={30} color={color} />
-            )}}
-        />
-    </Tab.Navigator>
-)
+function MyTabs() {
+    return (
+        <Tab.Navigator
+            initialRouteName="Home"
+            initialRoute={ParkingSpots} // Sørg for dette er initial route
+            activeColor="#fff"
+            shifting={true}
+            inactiveColor="#bbb"
+            barStyle={{backgroundColor: '#255CA2',}}
+        >
+            <Tab.Screen
+                name="Parkingspots"
+                component={TopNavigation}
+                options={{
+                    tabBarLabel: 'Parkeringer',
+                    tabBarColor: '#2568a2',
+                    tabBarIcon: ({color}) => (
+                        <Icon name="ios-car" color={color} size={24}/>
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Home"
+                component={HomeScreen}
+                options={{
+                    tabBarLabel: 'Hjem',
+                    tabBarColor: '#255ca2',
+                    tabBarIcon: ({color}) => (
+                        <Icon name="ios-home" color={color} size={24}/>
+                    ),
+                }}
+            />
+            <Tab.Screen
+                name="Profil"
+                component={StackNavigation}
+                options={{
+                    tabBarLabel: 'Settings',
+                    tabBarColor: '#2548a2',
+                    tabBarIcon: ({color}) => (
+                        <Icon name="ios-person" color={color} size={24}/>
+                    ),
+                }}
+            />
+        </Tab.Navigator>
+    );
+}
+
 
 const MainNavigation = ({auth}) => {
     return (
@@ -59,3 +74,4 @@ const MainNavigation = ({auth}) => {
 }
 
 export default MainNavigation
+
